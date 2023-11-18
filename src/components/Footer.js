a65e4add2ec4368879c7a347fb1c2b84e5095631
navyashree-acharaya <ksnavyashree10@gmail.com>
2023-11-18 11:12:09 +0530

    first commit

---
+++ src/components/Footer.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Footer() {
+  return (
+    <div>
+        <h6 id='foot'>Hello Everyone</h6 >
+    </div>
+  )
+}
+
+export default Footer
---
+++ src/components/Header.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import "./Header.css"
+
+function Header() {
+  return (
+    <div>
+        <h1 id='head'>Pen shop</h1>
+        <img src='https://3.imimg.com/data3/AW/KQ/MY-4902064/fountain-pen.jpg'width={100} height={100}></img>
+        </div>
+  )
+}
+
+export default Header
